3197eb0cd1ae11afd5df69a4ae657cbff542661b
Zepyhrus <sherkfung@gmail.com>
2024-07-05 13:49:45 +0800

    merge with Beijing

--- .bak/demo.py
+++ .bak/demo.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
   # frustums in the dataset
   # ======================================================================================================== #
   print("Estimating voxel volume bounds...")
-  n_imgs = 10
+  n_imgs = 1000
   cam_intr = np.loadtxt("data/camera-intrinsics.txt", delimiter=' ')
   vol_bnds = np.zeros((3,2))
   for i in range(n_imgs):
